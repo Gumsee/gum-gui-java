cd12c8f61822f008a2622aca5e2a63418c7e41ef
Gumse <gumse@gumse.de>
2022-11-25 01:29:57 +0100

    natives

--- run-example.sh
+++ run-example.sh
@@ -3,4 +3,5 @@
 EXAMPLE_NAME=$1
 shift
 
-mvn exec:java -pl example -Dexec.mainClass=com.gumse.$EXAMPLE_NAME -Dexec.args="$*"
+#mvn exec:java -pl example -Dexec.mainClass=com.gumse.$EXAMPLE_NAME -Dexec.args="$*"
+java -jar ./example/target/example-1.0.0.jar
