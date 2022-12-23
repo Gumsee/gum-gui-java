c4dc3e15fd854c0722d933a5da98ca50564f748c
Gumse <gumse@gumse.de>
2022-12-23 05:08:19 +0100

    .

--- run-example.sh
+++ run-example.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-EXAMPLE_NAME=$1
-shift
+#EXAMPLE_NAME=$1
+#shift
 
 #mvn exec:java -pl example -Dexec.mainClass=com.gumse.$EXAMPLE_NAME -Dexec.args="$*"
-java -jar ./example/target/example-1.0.0.jar
+java -jar ./example/target/example-1.0.0-jar-with-dependencies.jar
