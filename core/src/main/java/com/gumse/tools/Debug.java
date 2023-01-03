b0634c8149fa3296c1c72f1bd4d16df3ca49a158
Gumse <gumse@gumse.de>
2023-01-03 16:56:35 +0100

    fatal error level

--- core/src/main/java/com/gumse/tools/Debug.java
+++ core/src/main/java/com/gumse/tools/Debug.java
@@ -17,9 +17,10 @@ public class Debug
         formatter = new DecimalFormat("#0.00000");
     }
 
-    public static <T> void info(T str)  { System.out.println(getCurrentTime() + " (Info) "    + sPrefix + " " + str); }
-    public static void warn(String str)  { System.out.println(getCurrentTime() + " (Warning) " + sPrefix + " " + str); }
-    public static void error(String str) { System.out.println(getCurrentTime() + " (Error) "   + sPrefix + " " + str); System.exit(1); }
-    public static void debug(String str) { if(Globals.DEBUG_BUILD) {System.out.println(getCurrentTime() + " (Debug) " + sPrefix + " " + str); } }
-    private static String getCurrentTime() { return "[" + formatter.format((System.currentTimeMillis() - lStartTime) / 1000d) + "]"; }
+    public static <T> void info(T str)                   { System.out.println(getCurrentTime() + " (Info) "    + sPrefix + " " + str); }
+    public static void warn(String str)                  { System.out.println(getCurrentTime() + " (Warning) " + sPrefix + " " + str); }
+    public static void error(String str)                 { System.out.println(getCurrentTime() + " (Error) "   + sPrefix + " " + str); }
+    public static void fatal(String str, int returncode) { System.out.println(getCurrentTime() + " (Fatal) "   + sPrefix + " " + str); System.exit(returncode); }
+    public static void debug(String str)                 { if(Globals.DEBUG_BUILD) {System.out.println(getCurrentTime() + " (Debug) " + sPrefix + " " + str); } }
+    private static String getCurrentTime()               { return "[" + formatter.format((System.currentTimeMillis() - lStartTime) / 1000d) + "]"; }
 }
